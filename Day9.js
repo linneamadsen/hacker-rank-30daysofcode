0b9aad58cf0e30e73384b71c06131b60547697a9
Linnea <linneamadsen@gmail.com>
2021-03-25 14:19:59 -0500

    Create Day9.js
    See related Day9.js readme

---
+++ Day9.js
@@ -0,0 +1,47 @@
+'use strict';
+
+const fs = require('fs');
+
+process.stdin.resume();
+process.stdin.setEncoding('utf-8');
+
+let inputString = '';
+let currentLine = 0;
+
+process.stdin.on('data', inputStdin => {
+    inputString += inputStdin;
+});
+
+process.stdin.on('end', _ => {
+    inputString = inputString.replace(/\s*$/, '')
+        .split('\n')
+        .map(str => str.replace(/\s*$/, ''));
+
+    main();
+});
+
+function readLine() {
+    return inputString[currentLine++];
+}
+
+// My function below
+function factorial(n) {
+    var result = 1;
+    while (n > 0){
+        result = result * n;
+        n--
+    }
+    return (result);
+}
+// End of my function
+function main() {
+    const ws = fs.createWriteStream(process.env.OUTPUT_PATH);
+
+    const n = parseInt(readLine(), 10);
+
+    let result = factorial(n);
+
+    ws.write(result + "\n");
+
+    ws.end();
+}
